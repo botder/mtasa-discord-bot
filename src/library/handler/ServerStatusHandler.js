7b143e2408d0f4fd4bba18709687bb4edff789b0
Nick Spijker <33859387+Nick026@users.noreply.github.com>
2021-04-10 18:12:58 +0200

    Added playercount for mix and race

--- src/library/handler/ServerStatusHandler.js
+++ src/library/handler/ServerStatusHandler.js
@@ -1,19 +1,34 @@
-"use strict";
+'use strict';
 
-const Handler = require("./Handler");
+const Handler = require('./Handler');
+
+let race = {
+	player_count: 0,
+	max_players: 0
+};
+
+let mix = {
+	player_count: 0,
+	max_players: 0
+};
 
 class ServerStatusHandler extends Handler {
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        this.types.add("server.player_count");
-    }
+		this.types.add('server.player_count_race');
+		this.types.add('server.player_count_mix');
+	}
 
-    execute(bot, session, type, payload) {
-        if (type == "server.player_count") {
-            bot.setActivity(`${payload.player_count}/${payload.max_players}`);
-        }
-    }
+	execute(bot, session, type, payload) {
+		if (type === 'server.player_count_race') {
+			race = payload;
+		} else if (type === 'server.player_count_mix') {
+			mix = payload;
+		}
+
+		bot.setActivity(`Race: ${race.player_count}/${mix.max_players} | Mix: ${mix.player_count}/${mix.max_players}`);
+	}
 }
 
 module.exports = ServerStatusHandler;
